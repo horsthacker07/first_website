80a8c9ca917eccae111781e2431c3a580687e071
horsthacker07 <horsthacker07@gmail.com>
2025-04-11 19:01:45 +0200

    adding user-submit form and json-transfer

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ async function callAPI() {
     const data = await response.json();
     alert(data.message);
 }
+
+document.getElementById('userForm').addEventListener('submit', async (event) => {
+    event.preventDefault(); // Verhindert das Standard-Formularverhalten
+
+    const name = document.getElementById('name').value;
+
+    const response = await fetch('https://first-website-backend-spv7.onrender.com/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+    });
+
+    const result = await response.json();
+    alert(result.message);
+});
